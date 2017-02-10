ddecdbf86ed690c1582ba02a4de7ab1e2a65af47
I-am-sham <shamarculzac@gmail.com>
2017-02-10 14:10:32 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require("vendor/autoload.php");
+ 
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+ 
+require 'vendor/autoload.php';
+ 
+$app = new \Slim\App;
+$app->get('/', function (Request $request, Response $response) {
+ 
+	echo "hello World";
+});
+$app->run();
+?>
