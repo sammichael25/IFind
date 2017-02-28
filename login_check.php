3a90323c63d27b8db748f7eb4511f7631307df6d
safrazd <safrazd@gmail.com>
2017-02-28 12:39:55 -0400

    Created course object,course associative array
    So far was able to pull the required course information a user needed
    and stored that information in an array of objects.

--- login_check.php
+++ login_check.php
@@ -2,5 +2,7 @@
 include "lib.php";
 $email = $_POST['email'];
 $password=sha1 ($_POST['password']);
-login_check($email, $password);
+if (login_check($email, $password)==TRUE){
+    $courses = getCourses($email,$password); //filling courses array                 
+}
 ?>
