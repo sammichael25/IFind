af219abe7ef1cb410c8f61b422a2ba228f962614
safrazd <safrazd@gmail.com>
2017-03-11 23:54:51 -0400

    replaced gps column in building table with gpsLat and gpsLng

--- ifinddb.sql
+++ ifinddb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 06, 2017 at 07:07 AM
+-- Generation Time: Mar 12, 2017 at 04:53 AM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 7.0.13
 
@@ -32,7 +32,8 @@ DROP TABLE IF EXISTS `building`;
 CREATE TABLE `building` (
   `buildingId` int(20) NOT NULL,
   `buildingName` varchar(50) NOT NULL,
-  `GPS` int(50) NOT NULL,
+  `gpsLat` double(15,10) NOT NULL,
+  `gpsLng` double(15,10) NOT NULL,
   `facultyId` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -40,7 +41,7 @@ CREATE TABLE `building` (
 -- Dumping data for table `building`
 --
 
-INSERT INTO `building` (`buildingId`, `buildingName`, `GPS`, `facultyId`) VALUES(100, 'Natural Sciences Building', 0, 'FST');
+INSERT INTO `building` (`buildingId`, `buildingName`, `gpsLat`, `gpsLng`, `facultyId`) VALUES(100, 'Natural Sciences Building', 10.6409910000, -61.4002790000, 'FST');
 
 -- --------------------------------------------------------
 
@@ -169,6 +170,8 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`userId`, `fname`, `lname`, `email`, `password`, `departmentId`) VALUES(1, 'Safraz', 'Doolan', 'safraz@gmail.com', 'c80f5bc166cd6739ba9ba6d94acabc0aa01494da', 'DCIT');
 INSERT INTO `user` (`userId`, `fname`, `lname`, `email`, `password`, `departmentId`) VALUES(2, 'Michael', 'Sam', 'sam@gmail.com', '47b8015d98d5103a8a6981a979514855cac10ebc', 'DCIT');
 INSERT INTO `user` (`userId`, `fname`, `lname`, `email`, `password`, `departmentId`) VALUES(5004, 'Reza', 'Mohammed', 'reza@gmail.com', '3d4edf3cd249de9f27898c5b763e4b9a3e7aaa55', 'DCIT');
+INSERT INTO `user` (`userId`, `fname`, `lname`, `email`, `password`, `departmentId`) VALUES(5005, 'rice', 'summer', 'rice@gmail.com', '1ba95c1c3aacb805f7ee7b33dd8ba38657ea1bb4', 'DCIT');
+INSERT INTO `user` (`userId`, `fname`, `lname`, `email`, `password`, `departmentId`) VALUES(5006, 'Saffiyah', 'Doolan', 'saffiyahdoolan@gmail.com', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 'DCIT');
 
 -- --------------------------------------------------------
 
@@ -191,6 +194,9 @@ INSERT INTO `user_course` (`userId`, `courseCode`) VALUES(1, 'INFO 3425');
 INSERT INTO `user_course` (`userId`, `courseCode`) VALUES(1, 'INFO 3490');
 INSERT INTO `user_course` (`userId`, `courseCode`) VALUES(2, 'INFO 3410');
 INSERT INTO `user_course` (`userId`, `courseCode`) VALUES(5004, 'INFO 3410');
+INSERT INTO `user_course` (`userId`, `courseCode`) VALUES(5005, 'INFO 3410');
+INSERT INTO `user_course` (`userId`, `courseCode`) VALUES(5005, 'INFO 3425');
+INSERT INTO `user_course` (`userId`, `courseCode`) VALUES(5005, 'INFO 3490');
 
 --
 -- Indexes for dumped tables
@@ -261,12 +267,12 @@ ALTER TABLE `user_course`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `userId` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5005;
+  MODIFY `userId` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5007;
 --
 -- AUTO_INCREMENT for table `user_course`
 --
 ALTER TABLE `user_course`
-  MODIFY `userId` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5005;
+  MODIFY `userId` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5006;
 --
 -- Constraints for dumped tables
 --
